6f3c78feddf7acb62f4cda5b58fd687ee5a08547
adikc015 <adich3103@gmail.com>
2026-06-24 08:57:42 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Medical Chatbot',
+    version='0.0.0',
+    author="Aditya Kumar",
+    author_email="adich3103@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
